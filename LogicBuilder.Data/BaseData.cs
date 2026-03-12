ae94d4e43e90a4bbe0098c3bd5d820c0dff95875
Blaise Taylor <btaylor@bPolicySolutions.com>
2026-03-12 09:15:07 -0400

    Add project files.

---
+++ LogicBuilder.Data/BaseData.cs
@@ -0,0 +1,7 @@
+﻿namespace LogicBuilder.Data
+{
+    abstract public class BaseData
+    {
+        public EntityStateType EntityState { get; set; }
+    }
+}
---
+++ LogicBuilder.Data/EntityStateType.cs
@@ -0,0 +1,10 @@
+﻿namespace LogicBuilder.Data
+{
+    public enum EntityStateType
+    {
+        Unchanged,
+        Added,
+        Modified,
+        Deleted
+    }
+}
